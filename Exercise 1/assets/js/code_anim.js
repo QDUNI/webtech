fe0cb72a1de031be06618a8ae07517d5457e4f4e
Joep Kneefel <57983732+jpkneefel@users.noreply.github.com>
2020-02-18 17:36:09 +0100

    fixed bug

--- Exercise 1/assets/js/code_anim.js
+++ Exercise 1/assets/js/code_anim.js
@@ -1,101 +1,107 @@
-{
-    var header_content;
-    var canvas;
-    var ctx;
-    var width;
-    var height;
+var header_content;
+var canvas;
+var ctx;
+var width;
+var height;
 
-    document.addEventListener("DOMContentLoaded", () => {
-        header_content = document.getElementById("header_content");
+document.addEventListener("DOMContentLoaded", () => {
+    header_content = document.getElementById("header_content");
 
-        canvas = document.getElementById("code_anim");
-        ctx = canvas.getContext("2d");
-        width = 0;
-        height = 0;
+    canvas = document.getElementById("code_anim");
+    ctx = canvas.getContext("2d");
+    width = 0;
+    height = 0;
 
-        resize();
+    resize();
 
-        window.addEventListener("resize", onResize);
-    });
-
-    function onResize() {
-        resize();
-    }
-
-    function resize() {
-        canvas.width = document.body.clientWidth;
-        canvas.height = document.body.clientWidth * 0.3;
-
-        width = canvas.width;
-        height = canvas.height;
-    }
-
+    window.addEventListener("resize", onResize);
     window.addEventListener("scroll", onScroll);
+});
 
-    function onScroll() {
-        const position = document.body.scrollTop;
+function onResize() {
+    resize();
+}
 
-        header_content.setAttribute("style", "opacity: " + (100 - position * 0.4) + "%");
+function resize() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientWidth * 0.3;
 
-        anim_code(position);
+    width = canvas.width;
+    height = canvas.height;
+}
+
+function onScroll() {
+    let position = 0;
+
+    if (document.documentElement && document.documentElement.scrollTop)
+    {
+        position = document.documentElement.scrollTop;
+    } else if (document.body)
+    {
+        position = document.body.scrollTop;
     }
+    console.log(position);
 
-    function anim_code(pos) {
-        ctx.resetTransform();
-        ctx.clearRect(0, 0, width, height);
+    header_content.setAttribute("style", "opacity: " + (100 - position * 0.4) + "%");
 
-        // Calculate pivot and rotation
-        const stair_height = height / 8;
+    anim_code(position);
+}
 
-        const step = height / 8;
-        const offset = height / 0.8;
-        const i = pos < offset ? 1 : Math.ceil((pos - offset) / step);
-        const pivot = { x: 0.5 + stair_height * i + width * 0.7, y: height - 0.5 - stair_height * i };
-        const rot = ((pos - offset) / step) - Math.floor((pos - offset) / step);
+function anim_code(pos) {
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, width, height);
 
-        // Render code
-        ctx.fillStyle = "#3d3d3d";
-        ctx.font = Math.ceil(height * 0.1) + 'px monospace';
-        ctx.fillText("1. int step = " + (i - 1) + ";", width * 0.1, height * 0.5);
-        ctx.fillText("2. int rotation = " + Math.ceil(270 * rot) + ";", width * 0.1, height * 0.6);
+    // Calculate pivot and rotation
+    const stair_height = height / 8;
 
-        // Render stairs
-        ctx.strokeStyle = "#3d3d3d";
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        ctx.moveTo(0.5, height - 0.5);
+    const step = height / 8;
+    const offset = height / 0.8;
+    const i = pos < offset ? 1 : Math.ceil((pos - offset) / step);
+    const pivot = { x: 0.5 + stair_height * i + width * 0.7, y: height - 0.5 - stair_height * i };
+    const rot = ((pos - offset) / step) - Math.floor((pos - offset) / step);
 
-        for (let i = 0; i < 9; i++)
-        {
-            ctx.lineTo(stair_height * (i + 1) + width * 0.7 + 0.5, height - 0.5 - (stair_height * i));
-            ctx.lineTo(stair_height * (i + 1) + width * 0.7 + 0.5, height - 0.5 - (stair_height * (i + 1)));
-        }
-        ctx.stroke();
+    // Render code
+    ctx.fillStyle = "#3d3d3d";
+    ctx.font = Math.ceil(height * 0.1) + 'px monospace';
+    ctx.fillText("1. int step = " + (i - 1) + ";", width * 0.1, height * 0.5);
+    ctx.fillText("2. int rotation = " + Math.ceil(270 * rot) + ";", width * 0.1, height * 0.6);
 
-        // Render square
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
+    // Render stairs
+    ctx.strokeStyle = "#3d3d3d";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(0.5, height - 0.5);
 
-        ctx.translate(pivot.x, pivot.y);
-        ctx.rotate(degreesToRadians(180 * rot));
-
-        ctx.rect(0, 0, -stair_height, stair_height);
-
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "#efb501";
-        ctx.stroke();
-        ctx.fillStyle = "#efb501";
-        ctx.fill();
-
-
-
+    for (let i = 0; i < 9; i++)
+    {
+        ctx.lineTo(stair_height * (i + 1) + width * 0.7 + 0.5, height - 0.5 - (stair_height * i));
+        ctx.lineTo(stair_height * (i + 1) + width * 0.7 + 0.5, height - 0.5 - (stair_height * (i + 1)));
     }
+    ctx.stroke();
 
-    function degreesToRadians(degrees) {
-        return degrees * (Math.PI / 180);
-    }
+    // Render square
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
 
-    function radiansToDegrees(radians) {
-        return radians * (180 / Math.PI);
-    }
-};
+    ctx.translate(pivot.x, pivot.y);
+    ctx.rotate(degreesToRadians(180 * rot));
+
+    ctx.rect(0, 0, -stair_height, stair_height);
+
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "#efb501";
+    ctx.stroke();
+    ctx.fillStyle = "#efb501";
+    ctx.fill();
+
+
+
+}
+
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+function radiansToDegrees(radians) {
+    return radians * (180 / Math.PI);
+}
