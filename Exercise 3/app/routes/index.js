9525caf8d13177f260d9b16f1118b7d1ced3163b
Joep Kneefel <57983732+jpkneefel@users.noreply.github.com>
2020-03-19 10:09:59 +0100

    Added searchbar

--- Exercise 3/app/routes/index.js
+++ Exercise 3/app/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('pages/index', { title: 'Express' });
+});
+
+router.get("/search", function (req, res, next) {
+  res.render("pages/search", { title: "Search courses" });
 });
 
 module.exports = router;
