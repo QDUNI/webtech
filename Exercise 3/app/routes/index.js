d1232b4201e713e9adbf41ef88de1cb2aa85bac5
QDUNI <57755537+QDUNI@users.noreply.github.com>
2020-03-19 23:24:38 +0100

    add info for index.ejs

--- Exercise 3/app/routes/index.js
+++ Exercise 3/app/routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+    res.render('pages/index', { title: 'Express' });
 });
 
-router.get("/search", function (req, res, next) {
-  res.render("pages/search", { title: "Search courses" });
+router.get("/search", function(req, res, next) {
+    res.render("pages/search", { title: "Search courses" });
+});
+
+
+router.get("/signin", function(req, res, next) {
+    res.render("pages/signin", { title: "Sign in UU" });
 });
 
 module.exports = router;
