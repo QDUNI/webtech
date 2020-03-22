299fc8fad4017135c168f8eea02fff62c74282cc
Joep Kneefel <57983732+jpkneefel@users.noreply.github.com>
2020-03-22 12:00:05 +0100

    Added filter func

---
+++ Exercise 3/app/public/javascripts/search.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.getElementById("search__input");
+    const searchButton = document.getElementById("search__button");
+    const searchInfo = document.getElementById("search__info");
+    const filterIndicator = document.getElementById("filter__indicator");
+
+    const filters = document.getElementsByClassName("filter");
+
+    searchButton.onclick = () => {
+        alert("You searched: " + searchInput.value + ".");
+    };
+
+    const filterToggle = function (element) {
+        if (element.classList.contains("filter--toggled"))
+        {
+            element.setAttribute("class", "filter");
+        } else
+        {
+            element.classList.add("filter--toggled");
+        }
+
+        updateFilters();
+    };
+
+    for (var key in filters)
+    {
+        const element = filters[key];
+        filters[key].onclick = () => { filterToggle(element); };
+    }
+
+    function updateFilters() {
+        var numSelectedFilters = 0;
+
+        for (var i = 0; i < filters.length; i++)
+        {
+            if (filters[i].classList.contains("filter--toggled"))
+            {
+                numSelectedFilters++;
+            }
+        }
+
+        filterIndicator.innerHTML = numSelectedFilters;
+    }
+});
