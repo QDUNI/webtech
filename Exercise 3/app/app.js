cb83bcdb1671b79cc79e63ae9ca917a3007504d8
QDUNI <57755537+QDUNI@users.noreply.github.com>
2020-03-25 21:58:45 +0100

    add login server side

--- Exercise 3/app/app.js
+++ Exercise 3/app/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sqlite3 = require('sqlite3').verbose();
@@ -44,5 +45,8 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
+
 
 module.exports = app;
--- Exercise 3/app/routes/index.js
+++ Exercise 3/app/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database.db');
+var session = require('express-session');
+
+//session setup 
+router.use(session({
+    name: 'sid',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'ssshhhhh!.th1s,1s,a:s',
+    cookie: {
+        maxAge: 1000 * 60 * 30,
+        sameSite: true
+    }
+}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('pages/index', { title: 'Express' });
+    console.log(req.session.userid);
 });
 
 router.get("/search", function(req, res, next) {
@@ -13,6 +34,35 @@ router.get("/search", function(req, res, next) {
 
 router.get("/signin", function(req, res, next) {
     res.render("pages/signin", { title: "Sign in UU" });
+});
+
+router.post("/signin", function(req, res) {
+    const { studentid, password } = req.body
+    let sql = 'SELECT * FROM Students WHERE student_nr = ? AND password = ?';
+
+    if (studentid && password) {
+        db.get(sql, [studentid, password], (err, response) => {
+            if (err) {
+                return console.error(err.message);
+            } else {
+                if (response) {
+                    req.session.loggedin = true;
+                    req.session.userid = response.student_nr;
+                    res.redirect('/');
+
+
+                } else {
+                    res.send('incorrect Username and Password!');
+                    res.end();
+                }
+            }
+
+        })
+    } else {
+        res.send('Please enter Username and Password!');
+        res.end();
+    }
+
 });
 
 module.exports = router;
