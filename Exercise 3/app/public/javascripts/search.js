af52b1d6513ee59ef365c70efe856e53ca7afc77
Joep Kneefel <57983732+jpkneefel@users.noreply.github.com>
2020-03-29 10:55:33 +0200

    changes

--- Exercise 3/app/public/javascripts/search.js
+++ Exercise 3/app/public/javascripts/search.js
@@ -40,37 +40,107 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         filterIndicator.innerHTML = numSelectedFilters;
+
+        updateData();
     }
 
-    var data = document.getElementById('courses__data').textContent.trim();
-    var courses = JSON.parse(data);
-    console.log(courses);
+    updateData();
+    function updateData() {
+        const selectedFilters = document.getElementsByClassName("filter--toggled");
 
-    searchInfo.innerHTML = "We found " + courses.length + " courses";
+        var program = [];
+        var academicLevels = [];
+        var semester = [];
+        for (let i = 0; i < selectedFilters.length; i++)
+        {
+            switch (selectedFilters[i].innerHTML)
+            {
+                case "Computer_Science":
+                    program.push("Computer Science");
+                    break;
+                case "Mathematics":
+                    program.push("Mathematics");
+                    break;
+                case "Information_Science":
+                    program.push("Information Science");
+                    break;
+                case "level_1":
+                    academicLevels.push("level1");
+                    break;
+                case "level_2":
+                    academicLevels.push("level2");
+                    break;
+                case "level_3":
+                    academicLevels.push("level3");
+                    break;
+                case "1":
+                    semester.push(1);
+                    break;
+                case "2":
+                    semester.push(2);
+                    break;
+                case "3":
+                    semester.push(3);
+                    break;
+                case "4":
+                    semester.push(4);
+                    break;
+                default:
+                    break;
+            }
+        }
 
-    const searchResults = document.getElementById("search__results");
+        var data = document.getElementById('courses__data').textContent.trim();
+        var courses = JSON.parse(data);
 
-    for (let i = 0; i < courses.length; i++)
-    {
-        console.log(courses[i]);
-        const course = JSON.parse(courses[i]);
-        const coursePanel = document.createElement("div");
-        coursePanel.setAttribute("class", "course__panel");
 
-        const coursePanelImg = document.createElement("img");
-        const coursePanelDiv = document.createElement("div");
-        coursePanel.appendChild(coursePanelImg);
-        const coursePanelTitle = document.createElement("h2");
-        coursePanelTitle.innerHTML = course.title;
-        const coursePanelProgram = document.createElement("span");
-        coursePanelProgram.innerHTML = course.program + "<br><br>";
-        const coursePanelDescription = document.createElement("div");
-        coursePanelDescription.innerHTML = course.description;
-        coursePanelDiv.appendChild(coursePanelTitle);
-        coursePanelDiv.appendChild(coursePanelProgram);
-        coursePanelDiv.appendChild(coursePanelDescription);
-        coursePanel.appendChild(coursePanelDiv);
 
-        searchResults.appendChild(coursePanel);
+        const searchResults = document.getElementById("search__results");
+        searchResults.innerHTML = "";
+
+        let count = 0;
+        for (let i = 0; i < courses.length; i++)
+        {
+            const course = JSON.parse(courses[i]);
+            if (!program.includes(course.program))
+            {
+                console.log(course.course_id, course.program, course.ac_level, course.semester);
+                continue;
+            }
+            if (!academicLevels.includes(course.ac_level))
+            {
+                console.log(course.course_id);
+                continue;
+            }
+            if (!semester.includes(course.semester))
+            {
+                console.log(course.course_id);
+                continue;
+            }
+
+
+            const coursePanel = document.createElement("div");
+            coursePanel.setAttribute("class", "course__panel");
+
+            const coursePanelImg = document.createElement("img");
+            const coursePanelDiv = document.createElement("div");
+            coursePanel.appendChild(coursePanelImg);
+            const coursePanelTitle = document.createElement("h2");
+            coursePanelTitle.innerHTML = course.title;
+            const coursePanelProgram = document.createElement("span");
+            coursePanelProgram.innerHTML = course.program + "<br><br>";
+            const coursePanelDescription = document.createElement("div");
+            coursePanelDescription.innerHTML = course.description;
+            coursePanelDiv.appendChild(coursePanelTitle);
+            coursePanelDiv.appendChild(coursePanelProgram);
+            coursePanelDiv.appendChild(coursePanelDescription);
+            coursePanel.appendChild(coursePanelDiv);
+
+            searchResults.appendChild(coursePanel);
+
+            count++;
+        }
+
+        searchInfo.innerHTML = "We found " + count + " courses";
     }
 });
