9ebed9641b1df5de08426b0e303e1ec915de5c26
Joep Kneefel <57983732+jpkneefel@users.noreply.github.com>
2020-03-31 09:24:09 +0200

    navbar now gets background on scroll

--- Exercise 3/app/public/javascripts/index.js
+++ Exercise 3/app/public/javascripts/index.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementById("search__btn");
     const searchInput = document.getElementById("search");
+    const navWrapper = document.getElementsByClassName("nav__wrapper");
 
     searchButton.onclick = () => {
         window.location.href = "/search?value=" + searchInput.value;
     };
+
+    window.addEventListener("scroll", () => {
+        navWrapper[0].setAttribute("style", "background-color: hsla(0, 0%, 0%, " + (window.pageYOffset / 400) + ");");
+    });
 });
