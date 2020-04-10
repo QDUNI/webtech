0d41b13f7e4932671c0934ad222041ae0ff7f7c0
QDUNI <57755537+QDUNI@users.noreply.github.com>
2020-04-10 23:45:00 +0200

    test

--- Exercise 3/app/public/javascripts/validateeditprofile.js
+++ Exercise 3/app/public/javascripts/validateeditprofile.js
@@ -7,6 +7,11 @@ function validateEdit() {
     var level = document.getElementsByName("level")[0].value;
     var span = document.getElementById("edit__error");
 
+    console.log(repassword);
+    console.log(password);
+    return false;
+
+
     //check firstname
     if (firstname == "") {
         while (span.firstChild) {
